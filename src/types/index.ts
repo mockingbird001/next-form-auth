dd105725eca901894f3c7b67694432fa4b2b8376
mockingbird001 <bom_bom0123456789@hotmail.com>
2021-03-29 14:03:10 +0700

    signup to database

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,13 @@
-export type Role = 'CLIENT' | 'ITEMEDITOR' | 'ADMIN' | 'SUPERADMIN'
+export type Role = "CLIENT" | "ITEMEDITOR" | "ADMIN" | "SUPERADMIN";
 
 export interface User {
-  id: string
-  username: string
-  email: string
-  roles: Role[]
-  created_at: string
+  id: string;
+  username: string;
+  email: string;
+  roles: Role[];
+  created_at: string;
 }
+
+export type SignupArgs = Pick<User, "username" | "email"> & {
+  password: string;
+};
