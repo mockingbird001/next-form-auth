3907d2bcbf0d74c8b7a9ec14aebe7080694bec20
Mockingbird <hide0mockingbird@gmail.com>
2021-03-29 12:31:48 +0700

    signup react hook form

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react'
-import styled from 'styled-components'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm, ErrorMessage } from "react-hook-form";
 
-import Modal from './modal/Modal'
-import { AuthContext } from '../context/AuthContextProvider'
+import Modal from "./modal/Modal";
+import { AuthContext } from "../context/AuthContextProvider";
 
 interface Props {}
 
@@ -14,7 +15,7 @@ export const FormContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   margin: 2.5rem 0;
-`
+`;
 
 export const Header = styled.div`
   width: 100%;
@@ -27,7 +28,7 @@ export const Header = styled.div`
   h2 {
     margin: 0;
   }
-`
+`;
 
 export const StyledForm = styled.form`
   width: 100%;
@@ -41,7 +42,7 @@ export const StyledForm = styled.form`
     padding: 0;
     color: ${(props) => props.theme.colors.teal};
   }
-`
+`;
 
 export const InputContainer = styled.div`
   width: 100%;
@@ -50,7 +51,7 @@ export const InputContainer = styled.div`
   flex-direction: column;
   justify-content: stretch;
   align-items: stretch;
-`
+`;
 
 export const Input = styled.input`
   width: 100%;
@@ -62,7 +63,7 @@ export const Input = styled.input`
   font-size: 1.4rem;
   outline: none;
   box-shadow: 2px 2px 4px ${(props) => props.theme.colors.lightGrey};
-`
+`;
 
 export const Button = styled.button`
   width: 100%;
@@ -75,14 +76,14 @@ export const Button = styled.button`
   &:hover {
     background: ${(props) => props.theme.colors.darkTeal};
   }
-`
+`;
 
 export const StyledError = styled.p`
   margin: 0;
   padding: 0;
   color: red;
   font-size: 1.5rem;
-`
+`;
 
 export const StyledSwitchAction = styled.div`
   margin: 0;
@@ -96,7 +97,7 @@ export const StyledSwitchAction = styled.div`
     margin: 0;
     margin-top: 1rem;
   }
-`
+`;
 
 export const StyledInform = styled.div`
   margin: 0;
@@ -108,7 +109,7 @@ export const StyledInform = styled.div`
     color: ${(props) => props.theme.colors.teal};
     padding: 0;
   }
-`
+`;
 
 export const StyledSocial = styled.div`
   margin: 1rem auto;
@@ -145,16 +146,25 @@ export const StyledSocial = styled.div`
       background: ${(props) => props.theme.colors.googleDarkRed};
     }
   }
-`
+`;
 
 export const Divider = styled.hr`
   background-color: ${(props) => props.theme.colors.lightGrey};
   height: 1px;
   width: 100%;
-`
+`;
 
 const SignUp: React.FC<Props> = () => {
-  const { handleAuthAction } = useContext(AuthContext)
+  const { handleAuthAction } = useContext(AuthContext);
+  const { register, errors, handleSubmit } = useForm<{
+    username: string;
+    email: string;
+    password: string;
+  }>();
+
+  const submitSignup = handleSubmit(({ username, email, password }) => {
+    console.log(username, ": ", email, ": ", password);
+  });
 
   return (
     <Modal>
@@ -164,70 +174,108 @@ const SignUp: React.FC<Props> = () => {
         </Header>
 
         <StyledSocial>
-          <button className='facebook'>
-            <FontAwesomeIcon icon={['fab', 'facebook-f']} size='lg' />
+          <button className="facebook">
+            <FontAwesomeIcon icon={["fab", "facebook-f"]} size="lg" />
             <a>Sign in with Facebook</a>
           </button>
-          <button className='google'>
-            <FontAwesomeIcon icon={['fab', 'google']} />
+          <button className="google">
+            <FontAwesomeIcon icon={["fab", "google"]} />
             <a>Sign in with Google</a>
           </button>
         </StyledSocial>
 
         <Divider />
 
-        <StyledForm>
-          <p className='email_section_label'>or sign up with an email</p>
+        <StyledForm onSubmit={submitSignup}>
+          <p className="email_section_label">or sign up with an email</p>
           <InputContainer>
             <label>Username</label>
             <Input
-              type='text'
-              name='username'
-              id='username'
-              placeholder='Your username'
-              autoComplete='new-password'
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Your username"
+              autoComplete="new-password"
+              ref={register({
+                required: "Username is required.",
+                minLength: {
+                  value: 3,
+                  message: "Username must be at least 3 characters.",
+                },
+                maxLength: {
+                  value: 60,
+                  message: "Username must be not more than 60 characters.",
+                },
+              })}
             />
+            <ErrorMessage errors={errors} name="username">
+              {({ message }) => <StyledError>{message}</StyledError>}
+            </ErrorMessage>
           </InputContainer>
 
           <InputContainer>
             <label>Email</label>
 
             <Input
-              type='text'
-              name='email'
-              id='email'
-              placeholder='Your email'
-              autoComplete='new-password'
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Your email"
+              autoComplete="new-password"
+              ref={register({
+                required: "Email is required.",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Email is in wrong format.",
+                },
+              })}
             />
+            <ErrorMessage errors={errors} name="email">
+              {({ message }) => <StyledError>{message}</StyledError>}
+            </ErrorMessage>
           </InputContainer>
 
           <InputContainer>
             <label>Password</label>
 
             <Input
-              type='password'
-              name='password'
-              id='password'
-              placeholder='Your password'
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Your password"
+              ref={register({
+                required: "Password is required.",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters.",
+                },
+                maxLength: {
+                  value: 50,
+                  message: "Password must be not more than 50 characters.",
+                },
+              })}
             />
+            <ErrorMessage errors={errors} name="password">
+              {({ message }) => <StyledError>{message}</StyledError>}
+            </ErrorMessage>
           </InputContainer>
-          <Button disabled>Submit</Button>
+          <Button>Submit</Button>
         </StyledForm>
         <StyledSwitchAction>
           <p>
-            Already have account?{' '}
+            Already have account?{" "}
             <span
-              style={{ cursor: 'pointer', color: 'red' }}
-              onClick={() => handleAuthAction('signin')}
+              style={{ cursor: "pointer", color: "red" }}
+              onClick={() => handleAuthAction("signin")}
             >
               sign in
-            </span>{' '}
+            </span>{" "}
             instead.
           </p>
         </StyledSwitchAction>
       </FormContainer>
     </Modal>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { AppProps } from 'next/app'
+import React from "react";
+import { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
 
-import Layout from '../components/Layout'
-import AuthContextProvider from '../context/AuthContextProvider'
+import Layout from "../components/Layout";
+import AuthContextProvider from "../context/AuthContextProvider";
+import { client } from "../apollo/client";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthContextProvider>
-  )
+    <ApolloProvider client={client}>
+      <AuthContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthContextProvider>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
