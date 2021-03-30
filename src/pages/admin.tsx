aa319086f8ec0f93ad33e5f88172ec0a2cf0121c
mockingbird001 <bom_bom0123456789@hotmail.com>
2021-03-30 11:03:11 +0700

    protect router admin

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,29 @@
-import Admin from '../components/Admin'
+import { useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import Loader from "react-loader-spinner";
+import { AuthContext } from "../context/AuthContextProvider";
+import { isAdmin } from "../helpers/authHelpers";
+import Admin from "../components/Admin";
 
 export default function AdminPage() {
-  return <Admin />
+  const { loggedInUser } = useContext(AuthContext);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loggedInUser) {
+      router.push("/");
+    } else {
+      if (!isAdmin(loggedInUser)) {
+        router.push("/dashboard");
+        alert("No Authorization.");
+      }
+    }
+  }, [loggedInUser]);
+
+  return !isAdmin(loggedInUser) ? (
+    <Loader type="Oval" color="teal" height={30} width={30} timeout={30000} />
+  ) : (
+    <Admin />
+  );
 }
